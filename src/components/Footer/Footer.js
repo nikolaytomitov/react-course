ee0cecff75a0ee133189f25a4f69a86ed7a66202
Nikolay Tomitov <nikolay.tomitov@ittalents.bg>
2018-06-15 09:30:00 +0300

    commitnah

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './Footer.css'
 
 const footer = (props) => {
     let total = props.orders.reduce((sum, order) =>
@@ -11,11 +11,25 @@ const footer = (props) => {
         nadpis = <h4> Vsichko nakupi, jenata shte e dovolna </h4>;
     }
 
+    const style = {
+        fontSize: '20px',
+        color: 'blue',
+        border: '1px solid black',
+        padding: '10px',
+        margin: '10px',
+        boxShadow: '5px 5px 5px yellow'
+    }
+
+    if (total > 100) {
+        style.color = 'red';
+        style.fontSize = '30px';
+    }
+
     return (
-        <footer>
+        <footer className='Footer'>
             {nadpis}
 
-            <span>Smetkata ti e: {total}</span>
+            <span style={style}>Smetkata ti e: {total}</span>
         </footer>
     )
 }
